1d3c1dd7b09577064fcd20069438f4b79ffc86e1
Ankit Patel <ankitpatel1984@gmail.com>
2023-10-30 17:19:41 +0000

    instantvariable

---
+++ InstantVariable.java
@@ -0,0 +1,16 @@
+package Instancevariable;
+
+public class InstantVariable {
+
+    int a=10;
+    void method1(){
+
+    }
+    int b=20;
+    void method2(){
+
+    }
+    public static void main (String[] args){
+
+    }
+}
